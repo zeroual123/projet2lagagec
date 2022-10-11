611135da636714911923944a1f8adb112fe91e2c
zeroual123 <114814862+zeroual123@users.noreply.github.com>
2022-10-11 22:48:44 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -1,91 +1,65 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define pi 3.14
-
+  const float _pi =3.14;  // les constants
 int main()
 {
-    printf("mohamed  \n zeroual");
 
-     printf("bonjour je suis %s \n" , "mohamed");
-      printf("j'ai %d ans \n" ,18);
+    printf("bonjour je suis %s , j'ai %.2f ans ", "mohamed ", 18.5);
+                            // octal
+    printf("\nle code octal pour %d est : %o \n ", 14,14);
+                            //hexadécimal
+     printf(" le code hexadecimal pour %d est: %x \n", 16 , 16);
+                            // les variables
+       int x ,y;
+       float z;
+       char prenom [8] = "moh\0med";
+       x = 15;
+       y = 13;
+       z = 1.5;
+        printf(" y =%d \n x = %d \n z =%.2f \n prenom = %s",x ,y ,z,prenom);
+                            // les constantes
 
-       printf("salut je suis %s , j'ai %.2f   ans \n", "mohamed zeroual",18.558);
+        float rayon = 4 , surface;
+        surface  = rayon * rayon * _pi;
+        printf("\nla surface est %.2f", surface);
+                            //les opérateurs arithmétiques
+         int p = 10 , o = 2 , r ;
+        r = p % o;
+        printf (" \n%d", r );
+                            // les opérateurs d'assignation
+          x = 10;
+          x +=2;
+          printf("\n %d",x);
 
-        printf("le cod octal pour %d est: %o \n",14 , 14);
+                            //les opérateurs conditionnel
+            int m =100,a =55,e;
+            e = (m >= a)? 1 : 0;
+            printf("\n%d",e);
+                            // logique
+            e = !(m > a ||a > m)? 1:0;
+            printf("\n%d", e);
+                            // d'incrémentation
+             x =100;
+             printf  ("\n%d",x);
+             x++;
+             printf  ("\n%d",x);
+             x--;
+             printf  ("\n%d",x);
+                            // scanf
 
-        int y,x;
-        float z = 3.2;
-        char prenom[] = "mohamed";
-        x=10;
-        y = 12;
-        printf("y = %d \n x = %d \n z = %.2f \n prenom = %s",y,x,z,prenom);
+             int w,q,u;
+             printf("\nentrer la valeur de w: \t");
+             scanf("\n%d",&w);
+             printf("\nentrer la valeur de q: \t");
+             scanf("\n%d",&q);
+             printf("Resultat de w +q: %d", w+q);
 
-                                   /* constante */
-        float rayon = 4,surface;
-        surface = rayon*rayon*pi;
-        printf("la surface est %.2f ", surface);
-
-                            /*caractre de fin de chaine*/
-        char jour[6] ="lu\0di";
-        printf("\n%s",jour);
-
-                            /* les opérateures arithmétiques*/
-        int h=10,e=2,r;
-         r=h+e;
-         printf("\n%d",r);
-
-                            /* les opérateures d'assignation*/
-         int t =15;
-         t+=5;
-         printf("\n%d",t);
-
-                            /* les opérateures conditionnel*/
-        int m = 48,k=6,p;
-        p = (m > k)? 1: 0;
-        printf("\n %d",p);
-                            /*cpmaraison*/
-
-         p = (m != k)? 1:0;
-         printf("\n%d",p);
-                            /*logiques*/
-
-         int l = 55 , d = 2 , n;
-         n = !(l > d || l  < 55)? 1 :0;
-         printf("\n%d",n);
-
-                            /*d'incémentation*/
-
-         int c=100;
-         printf("\n%d",c);
-         c++  ;
-         printf("\n%d",c);
-
-                            /* scanf */
-
-          printf("\nentrer la valeur de x: \t");
-          scanf("%d" ,&x);
-          printf("\nentrer la valeur de y: \t");
-          scanf("%d" ,&y);
-           printf("\n resultat%d",x+y);
-
-                            /* if* */
-              // pin a numbre between 1000 and 9999
-           int pin;
-           printf("\n enter your pin code: \t");
-           scanf("%d",pin);
-           if(pin==1888){
-            printf("\n unlocked!");
-
-            }else{
-                printf("please verify your pin code. \n");
-            }
+                            // switch
 
 
 
 
 
-
-
-        return 0;
-  }
+    return 0;
+}
 
